94657bdc5a97a5f3a62f808455425c9096e4fb54
Yashnarkar13 <narkaryash939@gmail.com>
2025-06-05 13:09:30 +0530

    learning

---
+++ console.js
@@ -0,0 +1,41 @@
+console.log("Hello World");
+let fill ='red';
+fill = 'blue';
+fill = 'green';
+console.log(fill);
+const color = 'yellow';
+console.log(color);
+let a=10
+if(a%2==0)
+{
+    console.log("a is even");
+}
+else{
+    console.log("a is not 10");
+}
+for(let i=0;i<5;i++)
+{
+    console.log(i);
+}
+while(a>0)
+{
+    console.log(a);
+    a--;
+}
+switch(fill)
+{   
+    case 'red':
+        console.log("The color is red");
+        break;
+    case 'blue':
+        console.log("The color is blue");
+        break;
+    case 'green':
+        console.log("The color is green");
+        break;
+    default:
+        console.log("Unknown color");
+}       
+
+
+
